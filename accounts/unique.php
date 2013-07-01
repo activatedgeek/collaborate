45f3bd2ada73bcc79f4911ee6e32e94ba367cceb
Sanyam Kapoor <ksanyam18@yahoo.com>
2013-07-01 20:31:08 +0530

    Create unique.php
    Echo-ing NULL

---
+++ accounts/unique.php
@@ -0,0 +1,31 @@
+<?php
+$con = mysqli_connect("localhost","root","123","collaborate");
+$resp = "OK";
+if(!$con){
+    $resp = 'DENY';
+    echo $resp;
+    mysqli_close($con);
+    return;
+}
+$username = $_POST['username'];
+$query = "SELECT username FROM user WHERE username='".$username."'";
+$result = mysqli_query($con, $query);
+if (!$result)
+{
+    $resp = 'DENY';
+    mysqli_close($con);
+    echo $resp;
+    return;
+}
+
+$number_of_rows = $result->num_rows;
+if ($number_of_rows > 0)
+{
+    $resp = 'DENY';    
+    mysqli_close($con);
+    echo $resp;
+    return;
+}
+
+echo $resp;
+?>
