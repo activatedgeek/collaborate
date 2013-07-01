0d77c227ac1eb2f628a767871238910dc5e1858a
Sanyam Kapoor <ksanyam18@yahoo.com>
2013-07-01 22:23:33 +0530

    Update validate.php

--- accounts/validate.php
+++ accounts/validate.php
@@ -1,21 +1,35 @@
 <?php
 $con = mysqli_connect("localhost","root","123","collaborate");
 $resp = "DENY";
-if (!$con){
+if(!$con){
     echo $resp;
+    mysqli_close($con);
     return;
 }
 $user = $_POST['user'];
 $pass = $_POST['pass'];
-$query = "SELECT password FROM user WHERE username=\'".$user."\'";
+$query = "SELECT password FROM user WHERE username='".$user."'";
 $pass = hash("sha512", $pass, false);
 $result = mysqli_query($con, $query);
 if (!$result)
 {
     echo $resp;
-    die('');
+    mysqli_close($con);
+    return;
+}
+while($row = mysqli_fetch_array($result)){
+    if($row['password'] == $pass){
+        $resp = 'OK';
+        mysqli_close($con);
+        echo $resp;
+        return;
+    }
+    else{
+        mysqli_close($con);
+        echo $resp;
+        return;
+    }
 }
 
-mysqli_close($con);
 echo $resp;
 ?>
