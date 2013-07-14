bae960512634936de3c1a2b324e7b40186a8933a
Sanyam Kapoor <ksanyam18@yahoo.com>
2013-07-14 22:31:32 +0530

    Create std_elements.php
    added navbar

---
+++ utils/std_elements.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script> -->
+    <title>Page structure</title>
+    <script src="//use.edgefonts.net/poiret-one.js"></script>
+    <script src="//use.edgefonts.net/miama:n4.js"></script>
+    <style>
+        body{
+            margin: 0;
+        }
+        #navbar{
+            padding-top: 5px;
+            position: fixed;
+            margin: auto 0.5%;
+            margin-top: -0.5em;
+            z-index: 1000;
+            width: 99%;
+            height: 50px;
+            border-radius: 20px;
+            border-top-right-radius: 7px;
+            border-top-left-radius: 7px;
+            -moz-box-shadow:    0 0 10px 3px #5c5d63;
+            -webkit-box-shadow: 0 0 10px 3px #5c5d63;
+            box-shadow:         0 0 10px 3px #5c5d63;
+            background: linear-gradient(rgba(10,10,10,0.85) 0,rgba(142,143,143,0.85) 60%,rgba(213,208,208,0.85) 100%, rgba(182,173,173,0.85) 90%);
+            overflow: hidden;
+          }
+        #navbar .navlinks{
+            text-align: center;
+            font-family: poiret-one, sans-serif;
+            font-weight: bold;
+            display: inline;
+            font-size: 29px;
+            transition: background 0.5s, text-shadow 0.5s;
+            -ms-transition: background 0.5s, text-shadow 0.5s;
+            -webkit-transition: background 0.5s, text-shadow 0.5s;
+        }
+        #navbar .nav{
+            display: inline-block;
+            height: 0.5em;
+            width: 4em;
+            min-height: 6em;
+            margin-top: -1.1em;
+            padding-top: 1em;
+            transition: background 0.5s;
+            -ms-transition: background 0.5s;
+            -webkit-transition: background 0.5s;
+        }
+        #navbar .nav:hover{
+            background: #9894a9;
+        }
+        #navbar .logo{
+            font-family: miama, cursive;
+            font-weight: bold;
+            font-size: 1.5em;
+            margin-left: 0.9em;
+            margin-right: 1em;
+        }
+        #navbar .navlinks a:link,a:visited,a:active{ 
+            color: #f1f0f0;
+            text-shadow: 0 3px 3px #303030;
+            text-decoration: none;
+        }
+        #navbar .logo a{
+            transition: text-shadow 0.5s;
+            -ms-transition: text-shadow 0.5s;
+            -webkit-transition: text-shadow 0.5s;
+        }
+        #navbar .logo a:hover{
+            text-shadow: 0 1px 3px #333;
+            color: #333131;
+        }
+        #navbar .dot{
+            color: rgb(255,255,255);
+            font-weight: bold;
+            font-size: 25px;
+            margin: 0 1em 0 1em;
+        }
+        #test{
+            margin-top: 30px;
+            height: 50em;
+            font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, "AppleGothic", sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="navbar">
+        <div id="1" class="logo navlinks"><a href="#">Collaborate</a></div><span class="dot">.</span>
+        <div id="2" class="navlinks nav"><a href="#">Link</a></div>
+        <div id="3" class="navlinks nav"><a href="#">Link</a></div>
+        <div id="4" class="navlinks nav"><a href="#">Link</a></div><span class="dot">.</span>
+        <div id="5" class="navlinks nav"><a href="#">Link</a></div>
+        <div id="7" class="navlinks nav"><a href="#">Link</a></div>
+    </div> 
+    <input type="text" id="test">
+</body>
+</html>
