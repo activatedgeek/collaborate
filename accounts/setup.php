b9fbb9c919a2bd5354beecfefe1166366ed6ad6d
Sanyam Kapoor <ksanyam18@yahoo.com>
2013-07-23 02:35:11 +0530

    Update setup.php

--- accounts/setup.php
+++ accounts/setup.php
@@ -11,8 +11,8 @@ $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $password = hash("sha512", $password, false);
-$values = "'".$username."','".$firstName."','".$lastName."','".$password."','".$email."'";
-$query = "INSERT INTO user(`username`,`first_name`,`last_name`,`password`,`email`) VALUES (".$values.")";
+$values = "'".$username."',NOW(),'".$firstName."','".$lastName."','".$password."','".$email."'";
+$query = "INSERT INTO user(`username`,`joined`,`first_name`,`last_name`,`password`,`email`) VALUES (".$values.")";
 if(!mysqli_query($con, $query)){
     echo mysqli_error($con);
     return;
