20734ba35c6827ec96d64fdc0213bcdf22203430
Sanyam Kapoor <ksanyam18@yahoo.com>
2013-07-29 00:31:19 +0530

    Update destroy.php

--- utils/destroy.php
+++ utils/destroy.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if($_POST['type']=='session'){
-  session_destroy();
+	session_destroy();
 	return;
 }
 $con = mysqli_connect("localhost","root","123","collaborate");
@@ -10,7 +10,9 @@ if(!$con){
     mysqli_close($con);
     return;
 }
+
 else if($_POST['type']=='project'){
+	//Delete all file relations
 $query = "DELETE FROM file WHERE project_id= (SELECT project_id FROM project WHERE title='".$_POST['title']."')";
 $result = mysqli_query($con,$query);
 if(!$result){
@@ -18,6 +20,7 @@ if(!$result){
 		mysqli_close($con);
 	    return;
 }
+//Delete project
 $query = "DELETE FROM project WHERE user_id=".$_SESSION['user']." AND title='".$_POST['title']."'";
 $result = mysqli_query($con,$query);
 if(!$result){
@@ -25,7 +28,47 @@ if(!$result){
 	mysqli_close($con);
     return;
 }
+//Delete project from directory
 
+}
+
+else if($_POST['type']=='file'){
+$query = "SELECT `path` FROM file WHERE file_id=".$_POST['id'];
+$result = mysqli_query($con,$query);
+if(!$result){
+	echo mysqli_error($con);
+	mysqli_close($con);
+    return;
+}
+$path = mysqli_fetch_array($result);
+
+$query = "DELETE FROM file WHERE file_id=".$_POST['id'];
+$result = mysqli_query($con,$query);
+if(!$result){
+	echo mysqli_error($con);
+	mysqli_close($con);
+    return;
+}
+
+//Update last project edit
+$query = "SELECT project_id AS pid FROM project WHERE title='".$_POST['title']."'";
+$result = mysqli_query($con,$query);
+if(!$result){
+	echo mysqli_error($con);
+	mysqli_close($con);
+    return;
+}
+$pid = mysqli_fetch_array($result);
+$query = "UPDATE project SET last_pmodified=NOW() WHERE project_id=".$pid['pid'];
+$result = mysqli_query($con,$query);
+if(!$result){
+	echo mysqli_error($con);
+	mysqli_close($con);
+    return;
+}
+//Delete file from directory
+$target = "../projects/".$_POST['title']."/".$path['path'];
+unlink($target);
 }
 
 if(isset($con))
