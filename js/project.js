40f5ae6261a84b1c3789c631fdbf6f628683f588
Sanyam Kapoor <ksanyam18@yahoo.com>
2013-08-01 02:26:06 +0530

    Update project.js
    updated basic proper working text editor!

--- js/project.js
+++ js/project.js
@@ -55,9 +55,9 @@ $(document).ready(function(){
 	/**Functionality of adding a new file***/
 	
 	/**************Handling text editor**********************/
-	var line;
-	var res_back=0;
-    var ctrlkey=false;
+	var line=1;
+    var old_filename = "";
+    var fid = 0;
 	$(".basename").click(function(){
 		var appendPath="";
 		if($(this).next().text()!='./'){
@@ -66,85 +66,141 @@ $(document).ready(function(){
 			appendPath = basename.join("/");
 			$("#dir").after("<span class='dir' id='subdir'>"+appendPath+"</span>");
 		}
+		var value= $(this).text();
+		old_filename = value;
+		fid = $(this).attr("fid").split("_")[1];
 		$.post("utils/loadfile.php",{path: $(".title").text()+"/"+appendPath+$(this).text()},function(data,status){
-			if(status=='success'){
-				$(".files").html(data).hide().fadeIn(500);
+			var fdata = data.split("&");
+			if(status=='success' && fdata[0]!='ERROR'){
+				$(".files").html(fdata[0]).hide().fadeIn(500);
 				$("#p_content").append("<input type='button' class='linkButton' value='Back' style='float:right;margin: 1em;font-size:1.5em;' id='back'>");
+				$("#new_file").fadeOut();
+				$("#code").val(fdata[1]);
+				var string = $("#code").val();
+				var count = string.match(/\n/g);
+				if(count == null){
+					line=1;
+					$("#code").attr("rows",line);
+				}
+				else{
+					line = count.length+1;
+					$("#code").attr("rows",line);
+				}
 			}
-			line = $("#line span").last().attr("id");
-			line = (line.split("_"))[1];
+			else if(status=='success' && data=='ERROR'){
+				alert('Error loading file: '+value);
+			}
+			$("#p_content").append("<span style='display: block;margin: 0.3em;font-size:2em;float: left'>Filename:</span><input type='text' id='fid_"+fid+"' class='genCreds filename' value='"+value+"'>");
 		});
 	});
+
 	$(document).on('click','#back',function(){ //back button to main files
 		window.location.reload();
+		$("#p_content span,#filename").fadeOut(500).remove();
 	});
-	$(document).on('keypress',"#code",function(event){
-            if(event.keyCode==13){ //enter key
-                line++;
-                $("#line").append("<span class='number' id='line_"+line+"'>"+line+"</span>");
-            }
-            //While the backspace or delete key is pressed, keep updating lines
-            else if(event.keyCode==8 || event.keyCode==46){ //backspace or delete key
-                    $.post("utils/line_check.php",{code: $(this).html()},function(data,status){
-                        if(status=='success'){
-                            res_back = data.split(" ");
-                        }
-                        if(line!=1 && res_back[0]==(line-1)){
-                            $("#line_"+line).remove();
-                            line--;
-                        }
-                    });
-            }
-        });
-		$(document).on('keyup',"#code",function(event){
-            //To handle mass delete of code
-            if((event.keyCode==8)  || (event.keyCode>=46 && event.keyCode<=90) || (event.keyCode>=96 && event.keyCode<=111) || (event.keyCode>=186 && event.keyCode<=192) || (event.keyCode>=219 && event.keyCode<=222) ){
-                if($(this).html()=='<br>'){
-                    $("#line_1").siblings().remove();
-                    line=1;
-                    return;
+
+	$(document).on('keydown','#code',function(event){
+		//constantly updated on keydown (keypress in general terms)!
+		if(event.keyCode==13){// ENTER KEY
+			line++;
+            $("#line").append("<span class='number' id='line_"+line+"'>"+line+"</span>");
+            $("#code").attr("rows",line);
+		}
+		else if(event.keyCode==8 || event.keyCode==46){//backspace or delete key
+			var string = $("#code").val();
+			var count = string.match(/\n/g);
+			if(count==null){
+				line=1;
+				$("#line_1").nextAll().remove();
+				$("#code").attr("rows",line);
+				return;
+			}
+			newcount = count.length;
+			if(newcount==line-2){
+				$("#line_1").siblings().last().remove();
+				line--;
+				$("#code").attr("rows",line);
+			}
+		}
+	});
+	
+	$(document).on('keyup','#code',function(){
+		var string = $("#code").val();
+		var count = string.match(/\n/g);
+		//full delete!
+		if(count==null){
+			line=1;
+			$("#line_1").nextAll().remove();
+			$("#code").attr("rows",line);
+			return;
+		}
+		var newl = count.length+1;
+		//for code pastes
+		if(newl>line){
+			var diff = newl-line;
+			for(var i=1;i<=diff+1;i++){
+				var newline = line+i;
+				$("#line").append("<span class='number' id='line_"+newline+"'>"+newline+"</span>");
+			}
+			line = newl+1;
+			$("#code").attr("rows",line);
+		}
+
+		//for sub-mass deletes
+		else if(newl < line){
+			$("#line_"+newl).nextAll().remove();
+			line = newl;
+			$("#code").attr("rows",line);
+		}
+	});
+
+    $(document).on('click','#save',function(){
+    	$.post("utils/save.php",{title: $(".title").text(),folder: $("#subdir").text(),old_name: old_filename,curr_name: $(".filename").val(),fid: fid,filedata: $("#code").val()},function(data,status){
+    		if(status=='success' && data!='OK')
+    			alert(data);
+    		else if(status=='success' && data=='OK'){
+    			$("#code").attr('readonly','readonly').css({'cursor':'none','background':'#eaeaea','color':'gray'});
+    			//alert('File Saved');
+    		}
+    	});
+    });
+    $(document).on('click','#edit',function(){
+    	$("#code").removeAttr('readonly').css({'cursor':'text','background':'#f0f3f6','color':'black'});
+    });
+    /***Compiler***/
+    $(document).on('click',"#compile",function(){
+        var file = $(".dir").text()+$(".filename").val();
+        var mode = $(this).attr('value');
+        $.post("utils/terminal.php",{file: file,mode: mode},function(data,status){
+                if(status=='success'){
+                    $("#run_res").hide().html(data).fadeIn();
+                    //alert(data);
                 }
-                $.post("utils/line_check.php",{code: $(this).html()},function(data,status){
-                        if(status=='success'){
-                            res_back = data.split(" ");
-                        }
-                        var diff=line-res_back[0];
-                        if(diff>0){
-                            var id = $("#line_1").siblings(":last").attr("id");
-                            id = id.split("_");
-                            id[1] -=diff;
-                            id = '#'+id.join("_");
-                            $(id).nextAll().remove();
-                            line = line-diff;
-                        }
-                    });
-            }
-            //To handle code pastes
-            if(event.keyCode==86 && ctrlkey){
-                $.post("utils/line_check.php",{code: $(this).html()},function(data,status){
-                        if(status=='success'){
-                            res_back = data.split(" ");
-                        }
-                        var diff = res_back[0] - line;
-                        if(diff>0){
-                            //diff+=5; //just some extra lines //still need better handle perhaps js would do
-                            for(var i=0;i<diff;i++){
-                                line++;
-                                $("#line").append("<span class='number' id='line_"+line+"'>"+line+"</span>");
-                            }
-                        }
-                        ctrlkey=false;
-                    });
-            }
-            if(event.ctrlKey){
-                ctrlkey=false;
-            }
-        });
-		
-        $(document).on('keydown',"#code",function(event){
-            if(event.ctrlKey)
-                ctrlkey=true;
-        });
+            });
+    });
+    $(document).on('click',"#run",function(){
+    	var file = $(".dir").text();
+    	var filename= ($(".filename").val()).split(".");
+    	if(filename[1]=='cpp' || filename[1]=='c' || filename[1]=='c++' || filename[1]=='cc' || filename[1]=='cxx'){
+    		filename[1] = 'exe';
+    		filename = filename.join(".");
+    	}
+    	else if(filename[1]=='java'){
+    		filename[1]='class';
+    		filename = filename.join(".");
+    	}
+    	else if(filename[1]=='py' || filename[1]=='pyw' || filename[1]=='pyc' || filename[1]=='pyo' || filename[1]=='pyd'){
+    		filename = filename.join(".");
+    	}
+    	file += filename;
+    	var mode = $(this).attr('value');
+    	$.post("utils/terminal.php",{file: file,mode: mode},function(data,status){
+                if(status=='success'){
+                    $("#run_res").hide().html(data).fadeIn();
+                    //alert(data);
+                }
+            });
+    });
     /**************Handling text editor**********************/
 
 	$(".delete").click(function(){
@@ -169,6 +225,7 @@ function valid(){
 	}
 	if(due!=''){
 		var c = checkdate();
+		alert(c);
 		if(c==1){
 			$("#due").css("border","solid red 2px");
 			$("#default_due").hide().css("color","red").fadeIn(1000).text("This is not a valid date format!");
